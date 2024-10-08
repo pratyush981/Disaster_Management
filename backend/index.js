0195b65283b8606ae9652ff63cf319e9fb545170
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2024-10-08 22:37:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const PORT = 3000; // Updated port
+const PORT = 3000; 
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
