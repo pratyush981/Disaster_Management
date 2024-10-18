64b2b562bb05364233f664520b464383b4373380
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2024-10-18 20:29:05 +0530

    Update process_model.py

--- backend/process_model.py
+++ backend/process_model.py
@@ -6,7 +6,6 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
-# Load the model
 loaded_model = load_model('model/modelkeywords.h5')
 
 data = {
